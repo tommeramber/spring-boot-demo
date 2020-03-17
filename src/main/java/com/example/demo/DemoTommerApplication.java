764950c2316174f015252a15b5d4d1d3ede9411f
tommer <tamber@redhat.com>
2020-03-17 14:24:42 +0200

    without postgresql

---
+++ src/main/java/com/example/demo/DemoTommerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTommerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTommerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import java.util.UUID;
+
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Person {
+    private final UUID id;
+
+    @NotBlank
+    private final String name;
+
+    public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
